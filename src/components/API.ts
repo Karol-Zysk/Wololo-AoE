58726e4b74486843346f219c29e3957809ba19e8
Karol Zyśk <trener.karolzysk@gmail.com>
2022-04-28 11:09:21 +0200

    diffrent api

--- src/components/API.ts
+++ src/components/API.ts
@@ -1,15 +1,22 @@
-
-
 export type UnitData = {
   name: string;
   id: number;
   armor: number;
   attack: number;
-  cost: { info: string; Cost: string; Wood: number; Gold: number; Food:number; Stone:number };
+  cost: {
+    info: string;
+    Cost: string;
+    Wood: number;
+    Gold: number;
+    Food: number;
+    Stone: number;
+  };
 };
 
 export const fetchUnitsData = async () => {
-  const response = await fetch("http://aoe2.devops.sages.pl/api/v1/units");
+  const response = await fetch(
+    "https://age-of-empires-2-api.herokuapp.com/api/v1/units"
+  );
   const data = await response.json();
   return data.units.filter(
     (unit: UnitData) => !unit.cost.info && unit.cost.Cost !== "No cost"
